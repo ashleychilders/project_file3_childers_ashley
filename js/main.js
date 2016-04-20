16cd120d406b7f3f7e22f87d64f55297ae2c41e5
Ashley Childers <ashley.childers@gmail.com>
2016-04-20 00:04:49 -0400

    Added google map jquery

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
 $( document ).ready( function() {
 
-//  $( function() {
-//    $( 'ul#menu li img' ).bubbleup();
-
   $( function() {
 
     // Custom Easing
     jQuery.scrollSpeed( 100, 250, 'easeOutCubic' );
-
   } );
 
+  var map = new GMaps( {
+  el: '#map',
+  lat: 36.6222889,
+  lng: -119.3229801
+} );
+
+map.addMarker( {
+  lat: 36.6222889,
+  lng: -119.3229801,
+  title: 'Pizza Planet',
+  click: function( e ) {
+    alert( 'Pizza Planet: Great Pizza, Great Times!' );
+  }
+} );
+define( [ 'async!http://maps.google.com/maps/api/js?v=3&sensor=false' ], function() {} ) ;
+
+require.config( {
+  paths: {
+    'googlemapsapi': 'googlemapsapi'
+  },
+  shim: {
+    gmaps: {
+      deps: [ 'googlemapsapi' ],
+      exports: 'GMaps'
+    }
+  }
+} );
 } );
